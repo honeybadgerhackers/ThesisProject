ea66c83e1cf77834c08007147cebf0319b348cf7
Patrick Brown Jr <pjbrn26@gmail.com>
2017-12-06 11:49:04 -0600

    attempting sagas with location

--- thesis-client/src/components/location.js
+++ thesis-client/src/components/location.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Platform, Text, View, StyleSheet, Button } from 'react-native';
 import { MapView, Constants, Location, Permissions } from 'expo';
 import Polyline from '@mapbox/polyline';
+import { join } from 'redux-saga/effects';
+import { getUserLocation } from '../actions/getUserLocation-action';
+import { getDirections } from '../actions/getDirections-action';
 
-export default class WayPoint extends Component {
+class WayPoint extends Component {
   state = {
-    location: null,
+    speed: null,
     errorMessage: null,
     disableButton: false,
     userLocation: true,
@@ -21,14 +25,13 @@ export default class WayPoint extends Component {
           'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
       });
     } else {
-      this._getLocationAsync();
+      this.props.getUserLocation();
+      // this._getLocationAsync();
     }
   }
 
   // * Default directions on screen - Testing purposes only, probably not useful
-  componentDidMount() {
- 
-  }
+  componentDidMount() {}
 
   // * This function is NECESSARY, unsubscribes to watchPosition to prevent memory leaks
   componentWillUnmount() {
@@ -43,79 +46,81 @@ export default class WayPoint extends Component {
   // * If I spent more time on this, I would break the wayPoint array
   // *  processing out to another function.
   _getDirections = async (origin, destination, joinedWaypoints) => {
-    if (!origin) {
-      // ! Just a test object to ensure functionality
-      const wayPointsObjects = this.state.wayPoints;
-    
-      /*
-        ! Way point array will probably come in a different format than
-        !  the one above.
-        ! This will deal with an object with JUST Latitude and Longitude.
-        ! Our Way points in the database will almost certainly have more
-        !  data on them.
-      */
-      const wayPoints = wayPointsObjects.map(wayPoint => Object.values(wayPoint).join());
-      // * Splice and save the first element
-      origin = wayPoints.splice(0, 1);
-      // * Same, but the last element.
-      destination = wayPoints.splice(wayPoints.length - 1, 1);
-      // * Google's directions endpoint takes way points separated by pipe character
-      joinedWaypoints = wayPoints.join('|');
-    }
-    try {
-      let resp;
-      // * Ali says all URLs should go in a config file. Maybe set these as a function
-      // *  that returns the correct one with the space filled in?
-      if (!joinedWaypoints) {
-        resp = await fetch(
-          `https://maps.googleapis.com/maps/api/directions/json?origin=${
-            origin
-          }&destination=${destination}&waypoints=${joinedWaypoints}`
-        );
-      } else {
-        resp = await fetch(
-          `https://maps.googleapis.com/maps/api/directions/json?origin=${
-            origin
-          }&destination=${destination}`
-        );
-      }
-      const respJson = await resp.json();
-      const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
-      const coords = points.map((point, index) => ({
-          latitude: point[0],
-          longitude: point[1]
-        }));
-      // * coords is saved in state to re-render the directions line.
-      this.setState({ coords });
-      return coords;
-    } catch (error) {
-      return error;
-    }
+  if (!origin) {
+    // ! Just a test object to ensure functionality
+    const wayPointsObjects = this.state.wayPoints;
+
+    /*
+      ! Way point array will probably come in a different format than
+      !  the one above.
+      ! This will deal with an object with JUST Latitude and Longitude.
+      ! Our Way points in the database will almost certainly have more
+      !  data on them.
+    */
+    const wayPoints = wayPointsObjects.map(wayPoint => Object.values(wayPoint).join());
+    // * Splice and save the first element
+    origin = wayPoints.splice(0, 1);
+    // * Same, but the last element.
+    destination = wayPoints.splice(wayPoints.length - 1, 1);
+    // * Google's directions endpoint takes way points separated by pipe character
+    joinedWaypoints = wayPoints.join('|');
+  }
+    this.props.getDirectionsSaga(origin, destination, joinedWaypoints);
+
+  // try {
+    //   let resp;
+    //   // * Ali says all URLs should go in a config file. Maybe set these as a function
+  //   // *  that returns the correct one with the space filled in?
+  //   if (!joinedWaypoints) {
+    //     resp = await fetch(
+  //       `https://maps.googleapis.com/maps/api/directions/json?origin=${
+  //         origin
+  //       }&destination=${destination}&waypoints=${joinedWaypoints}`
+  //     );
+  //   } else {
+  //     resp = await fetch(
+  //       `https://maps.googleapis.com/maps/api/directions/json?origin=${
+  //         origin
+  //       }&destination=${destination}`
+  //     );
+  //   }
+  //   const respJson = await resp.json();
+  //   const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+  //   const coords = points.map((point, index) => ({
+  //       latitude: point[0],
+  //       longitude: point[1]
+  //     }));
+  //   // * coords is saved in state to re-render the directions line.
+  //   this.setState({ coords });
+  //   return coords;
+  // } catch (error) {
+  //   return error;
+  // }
   };
 
   // * This fires off on page load, asks permission to access user location, and
   // *  sets the default location to it
-  _getLocationAsync = async () => {
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied'
-      });
-    }
+  // _getLocationAsync = async () => {
+  //   const { status } = await Permissions.askAsync(Permissions.LOCATION);
+  //   if (status !== 'granted') {
+  //     this.setState({
+  //       errorMessage: 'Permission to access location was denied'
+  //     });
+  //   }
 
-    const location = await Location.getCurrentPositionAsync({});
-    // * Sets current location to the users -
-    this.setState({ location });
-    // * Sets MAP current location.
-    this.setState({
-      region: {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }
-    });
-  };
+  //   const location = await Location.getCurrentPositionAsync({});
+  //   // * Sets current location to the users -
+  //   this.setState({ location });
+  //   // * Sets MAP current location.
+  //   this.setState({
+  //     region: {
+  //       latitude: location.coords.latitude,
+  //       longitude: location.coords.longitude,
+  //       latitudeDelta: 0.0922,
+  //       longitudeDelta: 0.0421
+  //     }
+  //   });
+  // };
 
   // * This currently fires off on simple button press, and tracks the users
   // *  location.
@@ -132,14 +137,20 @@ export default class WayPoint extends Component {
   };
 
   // * the callback for _trackLocationAsync
-  _handlePositionChange = location => {
+  _handlePositionChange = () => {
     // * pushes way points to a copy of the state's way points
-    const wayPoint = { lat: location.coords.latitude, lng: location.coords.longitude };
-
+    const wayPoint = {
+      lat: this.props.userLocation.coords.latitude,
+      lng: this.props.userLocation.coords.longitude
+    };
     const wayPoints = this.state.wayPoints.slice();
+    this.setState({ speed: this.props.userLocation.coords.speed });
     wayPoints.push(wayPoint);
+    console.log('WAYPOINT', wayPoint);
     // * Replaces way points in state, also current location
-    this.setState({ wayPoints, location: wayPoint });
+    this.setState({
+      wayPoints,
+    });
   };
 
   // * This will stop location services on button press and on component un-mount.
@@ -149,27 +160,34 @@ export default class WayPoint extends Component {
       this.setState({ disableButton: false });
     }
     this._getDirections();
+    this.setState({ coords: this.props.routeCoords });
+    console.log('STATE', this.state);
   };
 
   render() {
     let text = 'Waiting..';
     if (this.state.errorMessage) {
       text = this.state.errorMessage;
-    } else if (this.state.location) {
-      text = JSON.stringify(this.state.location);
+    } else if (this.props.userLocation) {
+      text = JSON.stringify(this.props.userLocation);
     }
 
+    console.log('ROUTE COORDS', this.props.routeCoords);
+    console.log('MAP REGION', this.props.mapRegion);
+    console.log('USER LOCATION', this.props.userLocation);
+    console.log('SPEED', this.state.speed);
     // * Just FYI, MapView.Polyline needs to be nested inside of MapView.
     // * Could probably be broken off into it's own components to make this
     // *  file less sprawling.
     // * Watch location enables location tracking, Stop Watching turns it off
-    // *  and also fires off the 'render way points' function. 
+    // *  and also fires off the 'render way points' function.
     return (
       <View style={styles.container}>
         <MapView
           style={{ flex: 7 }}
-          initialRegion={this.state.region}
+          // region={this.props.mapRegion}
           showsUserLocation={this.state.userLocation}
+          followsUserLocation={this.state.userLocation}
         >
           <MapView.Polyline
             coordinates={this.state.coords}
@@ -184,20 +202,39 @@ export default class WayPoint extends Component {
             onPress={this._trackLocationAsync}
           />
           <Button title="Stop Watching" onPress={this._stopTrackLocation} />
-          <Text style={styles.paragraph}>{text}</Text> 
+          <Text style={styles.paragraph}>Speed: {this.state.speed}</Text>
+          <Text style={styles.paragraph}>{text}</Text>
         </View>
       </View>
-    );
+      );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    userLocation: state.userLocation,
+    mapRegion: state.mapRegion,
+    routeCoords: state.routeCoords.coordsArray
+  };
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserLocation: () => {
+    dispatch(getUserLocation());
+  },
+  getDirectionsSaga: (origin, destination, joinedWaypoints) => {
+    dispatch(getDirections(origin, destination, joinedWaypoints));
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
   paragraph: {
-    margin: 24,
     fontSize: 18,
     textAlign: 'center'
   }
 });
+
+export default connect(mapStateToProps, mapDispatchToProps)(WayPoint);
--- thesis-client/src/sagas/sagas.js
+++ thesis-client/src/sagas/sagas.js
@@ -1,32 +1,115 @@
 import { all, call, put, takeEvery } from 'redux-saga/effects';
+import { MapView, Constants, Location, Permissions } from 'expo';
 import axios from 'axios';
-//worker saga - calls API and returns response
+import Polyline from '@mapbox/polyline';
+import  googleAPIKEY from '../config';
+
 
 const getTripsAsync = function* () {
   try {
-    // console.log('Attempting to get trips from API'); 
     const tripsRequest = yield call(axios.get, 'http://18.216.220.101:8091/route');
     yield put({ type: 'GET_TRIPS_SUCCESS', payload: tripsRequest.data });
-    // console.log('Saga Trips loaded');
   } catch (error) {
     console.log(error);
   }
 };
-//watcher saga - listen for actions to be dispatched, will call worker
 
 const watchGetTrips = function* () {
-  // console.log('redux saga is running the getTrips action');
   yield takeEvery('GET_TRIPS', getTripsAsync);
-  // console.log('Getting Trips');
 };
 
-//combine watcher sagas to root saga
+const getUserLocationAsync = function* () {
+  try { 
+    console.log('ASKING FOR LOCATION ACCESS PERMISSION');
+    const { status } = yield call(Permissions.askAsync, Permissions.LOCATION);
+    if (status !== 'granted') {
+      yield put({ type: 'GET_USER_LOCATION_FAILED', payload: 'Permission to access location was denied' });
+    }
+    const userLocation = yield call(Location.getCurrentPositionAsync, {});
+    // console.log('USER LOCATION', userLocation);
+    // * Sets current location to the users -
+    yield put({ type: 'UPDATE_MAP_REGION', 
+      payload: { 
+        latitude: userLocation.coords.latitude, 
+        longitude: userLocation.coords.longitude,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05
+        } });
+    yield put({ type: 'GET_USER_LOCATION_SUCCESS', payload: userLocation });
+    // * Sets MAP current location.    
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-//entry point to start all sagas at once
+const watchGetUserLocation = function* () {
+  console.log('GETTING USER LOCATION SAGA STARTED!');
+  yield takeEvery('GET_USER_LOCATION', getUserLocationAsync);
+};
+
+const getUserDirectionsAsync = function* (action) {    
+  // if (!origin) {
+  //     // ! Just a test object to ensure functionality
+  //     const wayPointsObjects = this.state.wayPoints;
+    
+  //     /*
+  //       ! Way point array will probably come in a different format than
+  //       !  the one above.
+  //       ! This will deal with an object with JUST Latitude and Longitude.
+  //       ! Our Way points in the database will almost certainly have more
+  //       !  data on them.
+  //     */
+  //     const wayPoints = wayPointsObjects.map(wayPoint => Object.values(wayPoint).join());
+  //     // * Splice and save the first element
+  //     origin = wayPoints.splice(0, 1);
+  //     // * Same, but the last element.
+  //     destination = wayPoints.splice(wayPoints.length - 1, 1);
+  //     // * Google's directions endpoint takes way points separated by pipe character
+  //     joinedWaypoints = wayPoints.join('|');
+  //   }
+  const origin = action.payload.origin;
+  const destination = action.payload.destination;
+  const joinedWaypoints = action.payload.joinedWaypoints;
+
+  try {
+      let res;
+      // * Ali says all URLs should go in a config file. Maybe set these as a function
+      // *  that returns the correct one with the space filled in?
+      if (!joinedWaypoints) {
+        res = yield call(axios.get, `https://maps.googleapis.com/maps/api/directions/json?origin=${
+            origin
+          }&destination=${destination}&waypoints=${joinedWaypoints}&key=${googleAPIKEY}`);
+      } else {
+        res = yield call(axios.get, `https://maps.googleapis.com/maps/api/directions/json?origin=${
+            origin
+          }&destination=${destination}&key=${googleAPIKEY}`);
+      }
+      console.log(res);
+
+      const points = Polyline.decode(res.data.routes[0].overview_polyline.points);
+      const coords = points.map((point) => ({
+          latitude: point[0],
+          longitude: point[1]
+        }));
+
+      // * coords is saved in state to re-render the directions line.
+      yield put({ type: 'UPDATE_ROUTE_COORDS', payload: coords });
+
+      return coords;
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+const watchGetDirections = function* () {
+  console.log('GETTING DIRECTIONS SAGA STARTED!');
+  yield takeEvery('GET_DIRECTIONS', getUserDirectionsAsync);
+};
+
 const rootSaga = function* () {
   yield all([
-    watchGetTrips(),
+    watchGetTrips(), watchGetUserLocation(), watchGetDirections(),
   ]);
 };
 
-export { rootSaga, watchGetTrips };
+export { rootSaga, watchGetTrips, watchGetUserLocation };
