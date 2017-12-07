67eee6eeb02e78350f1401976f9abafe551bb905
Rob Aird <robkaird@gmail.com>
2017-12-07 15:23:37 -0600

    (feat) Async storage retrieve/store

--- thesis-client/App.js
+++ thesis-client/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './src';
 import LoginNavigator from './src/navigation/login-navigation';
-import RootNavigator from "./src/navigation/root-navigator";
+// import RootNavigator from "./src/navigation/root-navigator";
 
 
 export default class App extends Component {
@@ -10,7 +10,7 @@ export default class App extends Component {
     return (
       <Provider store={store}>
         <LoginNavigator />
-        <RootNavigator />
+        {/* <RootNavigator /> */}
       </Provider>
     );
   }
--- thesis-client/src/utilities/async-storage.js
+++ thesis-client/src/utilities/async-storage.js
@@ -1,9 +1,16 @@
 import { AsyncStorage } from 'react-native';
 
-// eslint-disable-next-line
 export async function storeItem(item, selectedValue) {
   try {
     await AsyncStorage.setItem(item, selectedValue);
+  } catch (error) {
+    console.log(`AsyncStorage error: ${error.message}`);
+  }
+}
+
+export async function getItem(item) {
+  try {
+    return await AsyncStorage.getItem(item);
   } catch (error) {
     console.log(`AsyncStorage error: ${error.message}`);
   }
