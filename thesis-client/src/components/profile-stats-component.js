b3374b23e0562dcc406fbf4b89be4721da3bbcd5
zacharygagnier <gzachary@gmail.com>
2017-12-06 19:21:47 -0600

    (add) profile stats page

--- thesis-client/src/components/profile-stats-component.js
+++ thesis-client/src/components/profile-stats-component.js
@@ -2,23 +2,16 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 
-const stats = {
-speed: 56,
-distance: 543,
-calories: 2455,
-total_time: 1231234,
-};
-
-const ProfileStats = () => Object.keys(stats).map(e => (
+const ProfileStats = () => stats.map((e, i, a) => (
   <View
   key={e}
   style={styles.title}
   >
-       <Text style={styles.textBox}>
-         {e}
-         {stats[e]}
-       </Text>
-   </View>
+    <Text style={styles.textBox}>
+      {e}
+      {a[e]}
+    </Text>
+  </View>
 ));
     
 const styles = StyleSheet.create({
