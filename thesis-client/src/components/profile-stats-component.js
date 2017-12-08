f92a87b26edb54524ecde7bd3cb4c0b727277a7b
Zachary Gagnier <gagnierzachary@gmail.com>
2017-12-08 15:36:45 -0600

    (add) basic reduce method to display stats

--- thesis-client/src/components/profile-stats-component.js
+++ thesis-client/src/components/profile-stats-component.js
@@ -1,25 +1,66 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+const ProfileStats = ({sessions}) => {
 
-const ProfileStats = ({sessions}) => sessions.map((e, i, a) => (
-  <View
-    key={`sessions${i}`}
-    style={styles.title}
-  >
-    <Text style={styles.textBox}>
-      {i}
-    </Text>
-  </View>
-));
-    
+  const total = sessions.reduce((prev, current, i, a) => {
+    prev.total_time = (prev.total_time || 0) + current.time;
+    prev.total_distance = (prev.total_distance || 0) + current.distance;
+    prev.total_calories = (prev.total_calories || 0) + current.calories;
+    if (i === a.length - 1) {
+      prev.total_sessions = a.length;
+      prev.average_time = prev.total_time / a.length;
+      prev.average_distance = prev.total_distance / a.length;
+      prev.average_calories = prev.total_calories / a.length;
+    }
+    return prev;
+  }, {});
+
+  return  (
+    <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
+      <View style={styles.title}>
+        <Text style={styles.textBox}>
+          Total Distance: {total.total_distance}
+        </Text>
+      </View>
+      <View style={styles.title}>
+        <Text style={styles.textBox}>
+        Total Time: {total.total_time || 0}
+        </Text>
+      </View>
+      <View style={styles.title}>
+        <Text style={styles.textBox}>
+        Total calories: {total.total_calories || 0}
+        </Text>
+      </View>
+      <View style={styles.title}>
+        <Text style={styles.textBox}>
+        Average Distance: {total.average_distance || 0}
+        </Text>
+      </View>
+      <View style={styles.title}>
+        <Text style={styles.textBox}>
+        Average Time: {total.average_time || 0}
+        </Text>
+      </View>
+      <View style={styles.title}>
+        <Text style={styles.textBox}>
+        Average Calories: {total.average_calories || 0}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   title: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'blue',
+    width: 125,
+    height: 125,
+    backgroundColor: 'lightblue',
     borderRadius: 50,
     borderWidth: 2,
+    // flex: 1,
+    flexDirection: 'row',
   },
   textBox: {
     fontSize: 20,
