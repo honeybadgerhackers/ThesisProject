fb406bc17b7f46f15751c59f15b8f8cb304f59e5
Rob Aird <robkaird@gmail.com>
2017-12-08 11:07:53 -0600

    (refactor) Added test screen, for playing with features

--- thesis-client/App.js
+++ thesis-client/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './src';
 import LoginNavigator from './src/navigation/login-navigation';
+import TestingStuff from './src/screens/test';
 // import RootNavigator from "./src/navigation/root-navigator";
 
 
-export default class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <LoginNavigator />
-        {/* <RootNavigator /> */}
-      </Provider>
-    );
-  }
+export default function App() {
+  return (
+    <Provider store={store}>
+      <TestingStuff />
+      {/* <LoginNavigator /> */}
+      {/* <RootNavigator /> */}
+    </Provider>
+  );
 }
