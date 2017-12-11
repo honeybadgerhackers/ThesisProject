67af28e91e3c9710c626858e8a43c4d2351815d3
Zachary Gagnier <gagnierzachary@gmail.com>
2017-12-11 15:08:18 -0600

    (cleanup) unneccesary console log

--- thesis-client/src/components/favorites.js
+++ thesis-client/src/components/favorites.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import mapIcon from '../assets/icons/mapIcon.png';
 
 const Favorite = ({ navigate, favorites, showTripLocation }) => {
-    console.log(favorites);
   const goToMap = () => {
     navigate('Map');
   };
