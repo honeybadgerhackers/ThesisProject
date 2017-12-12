348b3ae5fd3dce8800b8f28cd08cc4549454bb17
Zachary Gagnier <gagnierzachary@gmail.com>
2017-12-11 23:04:20 -0600

    (refactor) get trips to accept a location and search distance

--- thesis-client/src/actions/getTrip-action.js
+++ thesis-client/src/actions/getTrip-action.js
@@ -1,7 +1,12 @@
 import { GET_TRIPS } from '../constants';
 
-export default () => {
+export default ({latitude, longitude}, distance = 0.07) => {
   return ({
     type: GET_TRIPS,
+    payload: {
+      lat: latitude,
+      lng: longitude,
+      distance,
+    },
   });
 };
