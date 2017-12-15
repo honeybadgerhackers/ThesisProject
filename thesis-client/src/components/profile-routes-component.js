35b4b38e82fd38020f29e5d36f3e87ae79ee6477
Rob Aird <robkaird@gmail.com>
2017-12-15 00:40:19 -0600

    (style) User's routes page

--- thesis-client/src/components/profile-routes-component.js
+++ thesis-client/src/components/profile-routes-component.js
@@ -1,95 +1,177 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import mapIcon from '../assets/icons/mapIcon.png'; 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Platform, View, Image, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import Rating from 'react-native-rating';
+import mapIcon from '../assets/icons/mapIcon.png';
+import { appColors, appColorsTransparency } from '../constants';
 
+const starIcons = {
+  filled: require('../assets/icons/star_filled.png'),
+  unfilled: require('../assets/icons/star_unfilled.png'),
+};
+
+const HeartIcon = () => (
+  <Ionicons
+    name="ios-heart"
+    size={35}
+    style={{color: appColors.citrine}}
+  />
+);
+
+const BackgroundHeartIcon = ({ favorite }) => (
+  <Ionicons
+    name="ios-heart"
+    size={40}
+    style={{
+      color: appColorsTransparency(1).lightBlue, position: 'absolute', right: 2, top: 1, margin: -4,
+    }}
+  />
+);
 
 const ProfileRoutes = ({routes, showTripLocation, navigate}) => {  
   const goToMap = () => {
     navigate('Map');
   };
   return routes.map((trip, i) => (
-    <View style={styles.container} key={`trip${i}`}>
-      <View style={styles.container} >
+    // eslint-disable-next-line
+    <ImageBackground
+      key={trip.id}
+      style={{}}
+      source={trip.photo_url ? { uri: trip.photo_url } : require('../assets/images/default.jpg')}
+    >
+      <View
+        style={styles.container}
+        key={trip.id}
+      >
         <View style={styles.textContainer}>
-          <Text style={styles.title}>{trip.route_name}</Text>
-          <Text style={styles.subtitle}>Current Rating: {trip.current_rating}</Text>
-          <Text style={styles.startingAddress}>12345 N.Starting Address</Text>
+          <Text style={styles.title}>{trip.display_name}</Text>
+          <Text style={styles.startingAddress}>{trip.route_name}</Text>
+          <View style={styles.subtitle}>
+            <Rating
+              initial={Math.floor(trip.current_rating)}
+              selectedStar={starIcons.filled}
+              unselectedStar={starIcons.unfilled}
+              maxScale={1.4}
+              starStyle={{
+                width: 17,
+                height: 17,
+              }}
+              editable={false}
+            />
+          </View>
         </View>
-        <View style={styles.imageContainer}>
-          <Text style={styles.imageContainerText}>{trip.type}</Text>
-          <TouchableOpacity
-            onPress={() => showTripLocation(trip, goToMap)}
-          >
-            <Image source={mapIcon} style={styles.imageStyle} />
-          </TouchableOpacity>
-          <Text style={styles.favoriteCount}>{trip.favorite_count}</Text>
+        <View style={styles.actionContainer}>
+          <View style={styles.imageContainer}>
+            <TouchableOpacity
+              onPress={() => showTripLocation(trip, goToMap)}
+            >
+              <Image
+                source={
+                  trip.route_preview ?
+                    { uri: trip.route_preview } :
+                    mapIcon
+                }
+                showIcon
+                style={styles.imageStyle}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.favoriteContainer}>
+            <BackgroundHeartIcon />
+            <HeartIcon />
+            <Text style={styles.favoriteCount}>{trip.favorite_count}</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </ImageBackground>
 ));
 };
 
+
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  flexDirection: 'row',
-  alignItems: 'flex-end',
-  justifyContent: 'flex-end',
-  backgroundColor: 'grey',
-  height: 150,
-  borderWidth: 2,
-},
-textContainer: {
-  flex: 3,
-  backgroundColor: 'grey',
-  height: 148,
-},
-title: {
-  fontSize: 20,
-  fontWeight: 'bold',
-  color: 'white',
-  textAlign: 'center',
-  marginTop: 30,
-},
-subtitle: {
-  marginTop: 10,
-  fontSize: 15,
-  color: 'white',
-  textAlign: 'center',
-},
-imageContainer: {
-  flex: 1,
-  backgroundColor: 'grey',
-  height: 148,
-  width: 50,
-},
-imageContainerText: {
-  textAlign: 'center',
-  fontSize: 15,
-  fontWeight: 'bold',
-  color: 'white',
-  marginTop: 30,
-},
-imageStyle: {
-  alignSelf: 'center',
-  marginTop: 5,
-  height: 75,
-  width: 75,
-},
-favoriteCount: {
-  marginTop: 2,
-  fontSize: 10,
-  color: 'white',
-  textAlign: 'center',
-  fontWeight: 'bold',
-},
-startingAddress: {
-  marginTop: 10,
-  fontSize: 15,
-  color: 'white',
-  textAlign: 'center',
-},
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    height: 157,
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+  textContainer: {
+    flex: 5,
+    borderRadius: 2,
+    padding: 5,
+    margin: 10,
+    backgroundColor: appColorsTransparency(0.7).navyBlue,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    backgroundColor: appColors.transparent,
+    color: appColors.lightBlue,
+    textAlign: 'left',
+    fontFamily: Platform.OS === 'ios' ? 'GurmukhiMN-Bold' : null,
+  },
+  startingAddress: {
+    marginTop: 3,
+    fontSize: 15,
+    backgroundColor: appColors.transparent,
+    color: appColors.lightBlue,
+    textAlign: 'left',
+  },
+  subtitle: {
+    marginTop: 3,
+    backgroundColor: appColors.transparent,
+  },
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 148,
+    width: 50,
+    backgroundColor: appColors.transparent,
+  },
+  actionContainer: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 9,
+    borderRadius: 100,
+    paddingTop: 5,
+    backgroundColor: appColors.transparent,
+  },
+  favoriteContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainerText: {
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: appColors.navyBlue,
+    marginTop: 30,
+  },
+  imageStyle: {
+    alignSelf: 'center',
+    marginTop: 5,
+    height: 75,
+    width: 75,
+  },
+  favoriteCount: {
+    position: 'absolute',
+    paddingLeft: 10,
+    paddingTop: 6,
+    paddingBottom: 10,
+    paddingRight: 10,
+    fontSize: 10,
+    color: appColors.lightBlue,
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
 });
 
 ProfileRoutes.propTypes = {
